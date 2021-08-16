d80c0dee814bfbfef833553a64a66f2effb07d67
dannythedeveloper1 <daniel.girma9@gmail.com>
2021-08-16 17:02:06 -0400

    ADD:utils

---
+++ client/src/utils/utils.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+let API_key = process.env.REACT_APP_WEATHER_API_KEY;
+
+export const currentWeatherData = async (city = "maryland") => {
+    const request = await axios.get(
+			`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_key}`
+    );
+    console.log(request.data);
+    return request;
+}
+
+export const dailyWeatherData = async (city = "maryland") => {
+	const request = await axios.get(
+		`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_key}`
+	);
+	console.log(request.data);
+	return request;
+};
