0a5cce94de8ff5b54b50388f83fe7c946debbb08
Franco <phanfranco@yahoo.com>
2021-08-18 14:29:23 -0700

    Created search bar with methods to save searched city weather data to state(current and daily forecast.)

--- client/src/Components/Search/Search.js
+++ client/src/Components/Search/Search.js
@@ -1,7 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
 
-const Search = () => {
-  return <div></div>;
+const SearchInput = styled.input`
+  height: 40%;
+  width: 85%;
+  border: none;
+  // display: none;
+`;
+
+const SearchButton = styled.button`
+  height: auto;
+  width: auto;
+  border-radius: 50%;
+  text-decoration: none;
+  padding:0
+  border: none;
+  background-color: blue;
+`;
+
+const SearchForm = styled.form`
+  height: 10%;
+  width: ;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0px 10px 0px 10px;
+`;
+
+const Search = (props) => {
+  const styles = {
+    margin: "0",
+    fontSize: "10px",
+  };
+
+  const [value, setValue] = useState("");
+
+  const onChange = (e) => {
+    console.log(e.target.value);
+    setValue(e.target.value);
+  };
+
+  return (
+    <SearchForm onSubmit={(e) => props.SearchSubmit(e, value)}>
+      <SearchInput
+        placeholder="Search by city name"
+        onChange={onChange}
+        type="text"
+      />
+
+      <SearchButton type="submit">
+        <p style={styles}>X</p>
+      </SearchButton>
+    </SearchForm>
+  );
 };
 
 export default Search;
