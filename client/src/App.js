50f7c9bdba94fc1d6654714a283fed162688e596
dannythedeveloper1 <daniel.girma9@gmail.com>
2021-08-19 13:17:28 -0400

    not ready yet

--- client/src/App.js
+++ client/src/App.js
@@ -8,79 +8,85 @@ import styled from "styled-components";
 import DailyForecast from "./Components/DailyForecast/DailyForecast";
 import Search from "./Components/Search/Search";
 import {
-  currentWeatherData,
-  dailyWeatherData,
-  getCurrentLocationWeather,
+	currentWeatherData,
+	dailyWeatherData,
+	getCurrentLocationWeather,
 } from "./utils/utils";
 
 function App() {
-  const [latitude, setLatitude] = useState();
-  const [longitude, setLongitude] = useState();
-  const [forcast, setForcast] = useState([]);
-  const [currentWeather, setCurrentWeather] = useState({});
-  const [dailyWeather, setDailyWeather] = useState({});
+	const [latitude, setLatitude] = useState();
+	const [longitude, setLongitude] = useState();
+	const [forcast, setForcast] = useState([]);
+	const [currentWeather, setCurrentWeather] = useState({});
+  const [dailyWeather, setDailyWeather] = useState();
+  const [isLoaded, setSetLoaded] = useState(false);
 
-  const successful = (position) => {
-    // console.log(position.coords);
-    setLatitude(position.coords.latitude);
-    setLongitude(position.coords.longitude);
-  };
+	const successful = (position) => {
+		// console.log(position.coords);
+		setLatitude(position.coords.latitude);
+		setLongitude(position.coords.longitude);
+	};
 
-  const error = (err) => {
-    console.log(err);
-  };
+	const error = (err) => {
+		console.log(err);
+	};
 
-  var options = {
-    enableHighAccuracy: true,
-    timeout: 5000,
-    maximumAge: 0,
-  };
+	var options = {
+		enableHighAccuracy: true,
+		timeout: 5000,
+		maximumAge: 0,
+	};
 
-  useEffect(() => {
-    // currentWeatherData().then(({ data }) => console.log("data", data));
-    dailyWeatherData().then(({ data }) =>
-      setForcast([...data.list.splice(0, 4)])
-    );
+	useEffect(() => {
+		// currentWeatherData().then(({ data }) => console.log("data", data));
+		dailyWeatherData().then(({ data }) =>
+			setForcast([...data.list.splice(0, 4)])
+		);
 
-    navigator.geolocation.getCurrentPosition(successful, error, options);
+		navigator.geolocation.getCurrentPosition(successful, error, options);
 
-    async function fetchData() {
-      await getCurrentLocationWeather(latitude, longitude);
-    }
+		async function fetchData() {
+			await getCurrentLocationWeather(latitude, longitude);
+		}
 
-    fetchData();
-    // console.log(latitude, longitude);
-  }, []);
+		fetchData();
+		// console.log(latitude, longitude);
+	}, []);
 
-  const SearchSubmit = async (e, value) => {
-    e.preventDefault();
-    console.log(value);
-    const weather = await currentWeatherData(value);
-    const dailyWeather = await dailyWeatherData(value);
-    console.log("current weather:", weather);
-    console.log("daily weather:", dailyWeather);
-    setCurrentWeather(weather.data);
-    setDailyWeather(dailyWeather.data);
-  };
+	const SearchSubmit = async (e, value) => {
+		e.preventDefault();
+		console.log(value);
+		const weather = await currentWeatherData(value);
+		const dailyWeather = await dailyWeatherData(value);
+		console.log("current weather:", weather);
+		console.log("daily weather:", dailyWeather);
+		setCurrentWeather(weather.data);
+		setDailyWeather(dailyWeather.data);
+	};
 
-  return (
+	return (
 		<div className="App">
 			<Main>
 				<Body>
-					<SoftBox height={"20%"}>
-						<h1>Hi</h1>
-					</SoftBox>
-					<SoftBox height={"50%"}>
-						<SoftBox height={"20%"}>{dailyWeatherData}</SoftBox>
-						<SoftBox height={"20%"}>{dailyWeatherData}</SoftBox>
-						<SoftBox height={"20%"}>{dailyWeatherData}</SoftBox>
-						<SoftBox height={"20%"}>{dailyWeatherData}</SoftBox>
-						<SoftBox height={"20%"}></SoftBox>
-						<SoftBox height={"20%"}></SoftBox>
-						<SoftBox height={"20%"}></SoftBox>
-						<SoftBox height={"20%"}></SoftBox>
-					</SoftBox>
-          <DailyForecast dailyWeather={dailyWeather.list}/>
+					{!dailyWeather ? (
+						<>
+							<SoftBox height={"20%"}>
+								<h1>Hi</h1>
+							</SoftBox>
+							<SoftBox height={"50%"}>
+								<SoftBox height={"20%"}>{dailyWeatherData}</SoftBox>
+								<SoftBox height={"20%"}>{dailyWeatherData}</SoftBox>
+								<SoftBox height={"20%"}>{dailyWeatherData}</SoftBox>
+								<SoftBox height={"20%"}>{dailyWeatherData}</SoftBox>
+								<SoftBox height={"20%"}></SoftBox>
+								<SoftBox height={"20%"}></SoftBox>
+								<SoftBox height={"20%"}></SoftBox>
+								<SoftBox height={"20%"}></SoftBox>
+							</SoftBox>
+						</>
+					) : (
+						<DailyForecast dailyWeather={dailyWeather.list} />
+					)}
 				</Body>
 				<SideBar>
 					<Search SearchSubmit={SearchSubmit} />
--- client/src/Components/DailyForecast/DailyForecast.js
+++ client/src/Components/DailyForecast/DailyForecast.js
@@ -3,21 +3,22 @@ import { DayStyle, DayIcon } from "./DayStyle";
 
 const DailyForecast = (props) => {
 	const [dayWeather, SetDayWeather] = useState({});
-	const [icon, setIcon] = useState('');
+	const [icon, setIcon] = useState("");
 	const [isLoaded, setIsLoaded] = useState(false);
+	SetDayWeather(props.dailyWeather);
 
-	useEffect(() => {
-		// dayData();
-	}, [props.dailyWeather])
-	console.log(props.dailyWeather);
+	// useEffect(() => {
+	// 	dayData();
+	// }, [props.dailyWeather]);
+	console.log(dayWeather);
 	const dayData = () => {
 		const data = [];
-		for (let i = 0; i < props.dailyWeather.length; i+8) {
-			data.push(props.dailyWeather[i]);			
+		for (let i = 0; i < dayWeather.length; i + 8) {
+			data.push(dayWeather[i]);
 		}
 		return data;
-	}	
-	// console.log(dayData());
+	};
+	console.log(dayData());
 
 	const styles = {
 		height: "30%",
@@ -26,13 +27,13 @@ const DailyForecast = (props) => {
 		display: "flex",
 		justifyContent: "space-around",
 		backgroundColor: "#bdc3c7",
+		borderRadius: "15px",
 	};
-
 
 	return (
 		<div style={styles}>
-			{isLoaded ? (
-				<>
+			{/* {isLoaded ? (
+				<> */}
 			<DayStyle>
 				Mon
 				<DayIcon src="http://openweathermap.org/img/wn/10d@2x.png" alt="" />
@@ -57,10 +58,10 @@ const DailyForecast = (props) => {
 				Fri
 				<DayIcon src="http://openweathermap.org/img/wn/10d@2x.png" alt="" />
 				75
-					</DayStyle>
-					</>
+			</DayStyle>
+			{/* </>
 			) : (
-				<div>hi</div>)}
+				<div>hi</div>)} */}
 		</div>
 	);
 };
