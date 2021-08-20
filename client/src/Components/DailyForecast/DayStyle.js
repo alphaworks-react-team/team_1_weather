16ee992000ab878807c3c01efe9c6526c1ccf8cd
dannythedeveloper1 <daniel.girma9@gmail.com>
2021-08-20 13:15:44 -0400

    Update: day style

--- client/src/Components/DailyForecast/DayStyle.js
+++ client/src/Components/DailyForecast/DayStyle.js
@@ -1,17 +1,21 @@
 import styled from "styled-components";
 
 export const DayStyle = styled.div`
-  height: 100%;
-  width: 18%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: space-around;
-  /* background: rgba(155, 155, 155, 0.25); */
-  box-shadow: 0 8px 32px 0 rgba(3, 3, 135, 0.2);
-  backdrop-filter: blur(4px);
-  -webkit-backdrop-filter: blur(4px);
-  border-radius: 10px;
+	height: 100%;
+	width: 18%;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: space-around;
+	/* background: rgba(155, 155, 155, 0.25); */
+	box-shadow: 0 8px 32px 0 rgba(3, 3, 135, 0.2);
+	backdrop-filter: blur(4px);
+	-webkit-backdrop-filter: blur(4px);
+	border-radius: 10px;
+	&:hover {
+		transform: scale(1.1);
+		cursor: pointer;
+	}
 `;
 
 export const DayIcon = styled.img.attrs((props) => ({
