4ec1e02b02d3070ce8a354c1964a765daa878dd0
Franco <phanfranco@yahoo.com>
2021-08-20 13:34:46 -0700

    fixing merge

--- client/src/Components/CurrentWeather/CurrentWeather.js
+++ client/src/Components/CurrentWeather/CurrentWeather.js
@@ -1,59 +1,3 @@
-<<<<<<< HEAD
-import styled from "styled-components";
-import React from "react";
-import Icons from "../Icons/Icons";
-import windy from "../../assets/23.png";
-import { TiWeatherWindy } from "react-icons/ti";
-
-const CurrentWeather = (props) => {
-  const styles = {
-    current: {
-      display: "flex",
-      flexDirection: "column",
-      justifyContent: "space-around",
-      width: "85%",
-      height: "50%",
-      borderRadius: "15px",
-      border: "2px solid rgb(101, 153, 254)",
-      backgroundColor: "rgb(152, 187, 255)",
-    },
-    icons: {
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      width: "100%",
-      height: "60px",
-      border: "none",
-    },
-    location: {
-      display: "flex",
-      justifyContent: "flex-start",
-      marginLeft: "5px",
-    },
-    temp: {
-      display: "flex",
-      justifyContent: "center",
-    },
-    description: {
-      display: "flex",
-      justifyContent: "center",
-    },
-  };
-  return (
-    <div style={styles.current}>
-      <div style={styles.location}>{props.location}</div>
-      <div style={styles.icons}>
-        <Icons description={props.description} />
-      </div>
-      <div style={styles.temp}>{props.temp}°</div>
-      <div style={styles.description}>{props.description}</div>
-      <div>
-        <div>
-          <TiWeatherWindy />
-          <img style={{ width: "30px", height: "30px" }} src={windy} />
-          wind
-          {/* {props.wind} */}
-=======
 import styled from 'styled-components'
 import React from 'react'
 import Icons from "../Icons/Icons.js"
@@ -148,7 +92,6 @@ const CurrentWeather = (props) => {
                     {props.humidity}
                 </div>
             </div>
->>>>>>> d8f9d9f42c091a2936153fc547dece227f22b5ec
         </div>
       </div>
       <div>Hum: {props.humidity}</div>
